a6126fd05eef012995369d2da3d231926d43fc8c
Delizald <david.elizalde.r.a@gmail.com>
2017-12-14 11:48:28 -0600

    day 1

---
+++ 30-days-of-code/1-Hello World.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in); 
+		String inputString = scan.nextLine(); 
+        scan.close(); 
+      
+		
+		System.out.println("Hello, World.");
+      
+	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
+        System.out.println(inputString);
+	}
+}
